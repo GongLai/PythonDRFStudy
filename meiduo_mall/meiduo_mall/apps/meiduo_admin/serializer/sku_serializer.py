7634982df6733f9634bf4ef985016376c673bfcc
Hx1213 <19879631220@163.com>
2019-07-31 22:18:36 +0800

    04商品管理--4.4 sku表信息添加/修改BUG修复[手动构建中间表，保存规格及选项信息]

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializer/sku_serializer.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializer/sku_serializer.py
@@ -20,12 +20,36 @@ class SKUSerializer(serializers.ModelSerializer):
     category_id = serializers.IntegerField()
     category = serializers.StringRelatedField()
 
-    # specs = SKUSpecsSerializer(many=True)
-    specs = SKUSpecsSerializer(many=True, read_only=True)
+    specs = SKUSpecsSerializer(many=True)
 
     class Meta:
         model = SKU
         fields = "__all__"
+
+    def create(self, validated_data):
+        """新建单一sku对象的时候，手动构建中间表数据，来保存规格和选项信息"""
+
+        specs = validated_data.pop("specs")
+        # 创建从表数据对象之前，先构建主表sku对象
+        instance = super().create(validated_data)
+        # 遍历规格信息
+        for temp in specs:
+            temp['sku_id'] = instance.id
+            SKUSpecification.objects.create(**temp)
+
+        return instance
+
+    def update(self, instance, validated_data):
+        specs = validated_data.pop("specs")
+        # 先删除中间表数据
+        SKUSpecification.objects.filter(sku_id=instance.id).delete()
+        # 根据新的规格选项新建
+        for temp in specs:
+            temp['sku_id'] = instance.id
+            SKUSpecification.objects.create(**temp)
+
+        # 实现sku对象更新
+        return super().update(instance, validated_data)
 
 
 class GoodsCategoriesSerializer(serializers.ModelSerializer):
